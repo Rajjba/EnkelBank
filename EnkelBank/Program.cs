a0dc7952d7e0a86cf15b1b17243d3ac23b5fd497
Rayan Hadjari <rayan.ha@me.com>
2022-09-07 14:50:11 +0200

    lagt till klasser

--- EnkelBank/Program.cs
+++ EnkelBank/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Kund {
+    String förNamn {get; set; }
+    String efterNamn;
+    List<String> accountNumber;
+    public String id;
+
+    public Kund(String förNamn, String efterNamn, String id) {
+        this.förNamn = förNamn;
+        this.efterNamn = efterNamn;
+        this.id = id;
+    }
+}
+
+public class Konto {
+    String ägare;
+    double saldo;
+    int senasteTransaktionen;
+
+    public Konto(String ägare, double saldo, int senasteTransaktionen) {
+        this.ägare = ägare;
+        this.saldo = saldo;
+        this.senasteTransaktionen = senasteTransaktionen;
+    }
+}
+
+public class Bank {
+    List<Kund> kunder;
+    List<Konto> konton;
+
+    public Bank() {
+        List<Kund> kunder = new List<Kund>();
+        List<Konto> konton = new List<Konto>(); 
+    }
+}
+
+static class Program{
+    static void Main() {
+
+        System.Console.Write("Välkommen till banken, vad vill du göra?"
+        + "\n" + "1. skapa ny kund"
+        + "\n" + "2. skapa nytt konto"
+        + "\n" + "3. koppla kund till konto"
+        + "\n" + "4. ändra saldo på konto"
+        + "\n" + "5. antal kunder" 
+        + "\n" + "6. totala bankens omsättning");
+
+    }
+}
